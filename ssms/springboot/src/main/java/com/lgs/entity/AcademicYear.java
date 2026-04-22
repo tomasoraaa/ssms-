9349f0c27f6bfa241b6485508f51605e1c43e266
tomasoraaa <82781201@qq.com>
2026-04-22 17:35:58 +0800

    refactor(entity): 使用Lombok简化AcademicYear实体类
    移除手动编写的getter/setter方法，改用@Data注解自动生成

--- ssms/springboot/src/main/java/com/lgs/entity/AcademicYear.java
+++ ssms/springboot/src/main/java/com/lgs/entity/AcademicYear.java
@@ -1,8 +1,11 @@
 package com.lgs.entity;
 
+import lombok.Data;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+@Data
 public class AcademicYear {
     private Integer id;
     private String year;
@@ -13,67 +16,4 @@ public class AcademicYear {
     private LocalDateTime selection_end;
     private Integer status;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
-
-    public Integer getSemester() {
-        return semester;
-    }
-
-    public void setSemester(Integer semester) {
-        this.semester = semester;
-    }
-
-    public LocalDate getStart_date() {
-        return start_date;
-    }
-
-    public void setStart_date(LocalDate start_date) {
-        this.start_date = start_date;
-    }
-
-    public LocalDate getEnd_date() {
-        return end_date;
-    }
-
-    public void setEnd_date(LocalDate end_date) {
-        this.end_date = end_date;
-    }
-
-    public LocalDateTime getSelection_start() {
-        return selection_start;
-    }
-
-    public void setSelection_start(LocalDateTime selection_start) {
-        this.selection_start = selection_start;
-    }
-
-    public LocalDateTime getSelection_end() {
-        return selection_end;
-    }
-
-    public void setSelection_end(LocalDateTime selection_end) {
-        this.selection_end = selection_end;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
 }
